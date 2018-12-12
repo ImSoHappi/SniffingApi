33aec7564523ea3b1fbe5d2f9f96eb666f8f679b
Max <max.lopezp@alumnos.duoc.cl>
2018-12-12 00:05:29 -0300

    Agrega correcion admin.py
    Se agrega los site.register para que dentro del administrador de django
    se puedan crear los modelos

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, Orden, Empleado
 
 admin.site.register( Cliente )
+admin.site.register (Orden)
+admin.site.register ( Empleado)
